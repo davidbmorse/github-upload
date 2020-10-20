f7f2c16f53d3f17385461a9e41eb24f97b7c0c5d
davidbmorse <dm701@cam.ac.uk>
2020-10-20 12:55:19 -0400

    initializing repository

---
+++ code_file_1.R
@@ -0,0 +1,10 @@
+#GIT upload test file
+#Code file 1
+
+x = 10
+y = 2
+
+z = x + y
+
+print(z)
+print(z/2)
---
+++ code_file_2.R
@@ -0,0 +1,4 @@
+#GIT upload test Function File
+#Code file 2
+
+print("Upload to github file version 1")
